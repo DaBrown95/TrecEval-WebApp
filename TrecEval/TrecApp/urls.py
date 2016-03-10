840bd01f38e49fccb090131d8a4598b73888fabd
DaBrown95 <david_a_brown@mac.com>
2016-03-10 13:20:50 +0000

    Added an about page
    Contains a link on the logo that returns back to home page, need to be
    fancier at some point…

--- TrecEval/TrecApp/urls.py
+++ TrecEval/TrecApp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 from TrecApp import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.home, name='home'))
+        url(r'^$', views.home, name='home'),
+        url(r'about/$', views.about, name='about'))
