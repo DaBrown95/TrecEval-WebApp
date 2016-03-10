41ed5dc9b14cd555717f921ca579498ca162b63a
David <david_a_brown@icloud.com>
2016-03-10 19:46:05 +0000

    Added a researcher model
    This model for researcher is almost complete. A foreign key needs to be
    added to ‘name’ once user accounts are set up

--- TrecEval/TrecApp/models.py
+++ TrecEval/TrecApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Researcher(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    url = models.URLField(max_length=200)
+    display_name = models.CharField(max_length=128)
+    organization = models.CharField(max_length=128)
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
