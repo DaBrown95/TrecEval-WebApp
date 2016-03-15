54fc016911804ebff52e2b461648e87a007b28c1
David <david_a_brown@icloud.com>
2016-03-15 18:29:06 +0000

    Merge branch 'master' into RobbBranch

--- TrecEval/TrecApp/admin.py
+++ TrecEval/TrecApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from TrecApp.models import Researcher, Track, Task, Run
 
 # Register your models here.
+admin.site.register(Researcher)
+admin.site.register(Track)
+admin.site.register(Task)
+admin.site.register(Run)
