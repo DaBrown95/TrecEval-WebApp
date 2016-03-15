b0cf682e7ef33b8fc0976a488ad46419868eece1
Noshin Saiyara <2167932s@student.gla.ac.uk>
2016-03-15 15:45:05 +0000

    Revert "TempCommit-Ignore"
    This reverts commit 1f96ea66a4ae13d02956d640ee1454363e0c74f8, reversing
    changes made to 132ea04572f7d467ab1458aa2b94423c42a6a359.

--- TrecEval/TrecApp/urls.py
+++ TrecEval/TrecApp/urls.py
@@ -3,8 +3,4 @@ from TrecApp import views
 
 urlpatterns = patterns('',
         url(r'^$', views.home, name='home'),
-        url(r'about/$', views.about, name='about'),
-		url(r'run/(?P<run_name_slug>[\w\-]+)/$', views.run, name='run'),
-		url(r'run/graph/$', views.graph, name='graph'),
-		url(r'researcher/(?P<researcher_name_slug>[\w\-]+)/$', views.researcher, name='researcher')
-		)
+        url(r'about/$', views.about, name='about'))
