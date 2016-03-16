0bbc69b09cdb85848c78e2d8473424a97b52c54b
DaBrown95 <david_a_brown@mac.com>
2016-03-16 17:35:42 +0000

    Revert "Fixed conflicts"
    This reverts commit c85a9bb7ae977b4461128c01469f7235bd66d1e1.

--- TrecEval/TrecApp/models.py
+++ TrecEval/TrecApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django_enumfield import enum
-from django.template.defaultfilters import slugify
 
 class run_type(enum.Enum):
     AUTOMATIC = 0
@@ -41,64 +40,36 @@ class feedback_type(enum.Enum):
 
 
 class Researcher(models.Model):
-	name = models.CharField(max_length=128, unique=True)
-	url = models.URLField(max_length=200)
+    name = models.CharField(max_length=128, unique=True)
+    url = models.URLField(max_length=200)
     # PROFILE PICTURE
-	display_name = models.CharField(max_length=128)
-	organization = models.CharField(max_length=128)
+    display_name = models.CharField(max_length=128)
+    organization = models.CharField(max_length=128)
 
-	slug = models.SlugField()
-
-	def save(self, *args, **kwargs):
-			# Uncomment if you don't want the slug to change every time the name changes
-			#if self.id is None:
-					#self.slug = slugify(self.name)
-			self.slug = slugify(self.name)
-			super(Researcher, self).save(*args, **kwargs)
-
-	def __unicode__(self):  #For Python 2, use __str__ on Python 3
-		return self.name
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
 
 
 class Track(models.Model):
-	title = models.CharField(max_length=128, unique=True)
-	track_url = models.URLField(max_length=200)
-	description = models.TextField()
-	genre = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, unique=True)
+    track_url = models.URLField(max_length=200)
+    description = models.TextField()
+    genre = models.CharField(max_length=128)
 
-	slug = models.SlugField()
-
-	def save(self, *args, **kwargs):
-			# Uncomment if you don't want the slug to change every time the name changes
-			#if self.id is None:
-					#self.slug = slugify(self.title)
-			self.slug = slugify(self.title)
-			super(Track, self).save(*args, **kwargs)
-
-	def __unicode__(self):      #For Python 2, use __str__ on Python 3
-		return self.title
+    def __unicode__(self):      #For Python 2, use __str__ on Python 3
+        return self.title
 
 
 class Task(models.Model):
-	track = models.ForeignKey(Track)
-	title = models.CharField(max_length=128)
-	#task_url = models.FileField()
-	description = models.TextField()
-	year = models.DateField()
-    #judgement_file = models.FileField()
+    track = models.ForeignKey(Track)
+    title = models.CharField(max_length=128)
+    tasl_url = models.URLField(max_length=200)
+    description = models.TextField()
+    year = models.DateField()
+    judgement_file = models.URLField(max_length=200)
 
-	slug = models.SlugField()
-
-	def save(self, *args, **kwargs):
-			# Uncomment if you don't want the slug to change every time the name changes
-			#if self.id is None:
-					#self.slug = slugify(self.title)
-			self.slug = slugify(self.title)
-			super(Task, self).save(*args, **kwargs)
-
-
-	def __unicode__(self):      #For Python 2, use __str__ on Python 3
-		return self.title
+    def __unicode__(self):      #For Python 2, use __str__ on Python 3
+        return self.title
 
 
 class Run(models.Model):
@@ -118,12 +89,3 @@ class Run(models.Model):
 
     def __unicode__(self):      #For Python 2, use __str__ on Python 3
         return self.name
-
-	slug = models.SlugField()
-
-	def save(self, *args, **kwargs):
-			# Uncomment if you don't want the slug to change every time the name changes
-			#if self.id is None:
-					#self.slug = slugify(self.name)
-			self.slug = slugify(self.name)
-			super(Run, self).save(*args, **kwargs)
--- TrecEval/TrecApp/views.py
+++ TrecEval/TrecApp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from TrecApp.forms import RunForm
+from TrecApp.models import Run
 from TrecApp.valueExtractor import *
-from TrecApp.models import Run, Researcher
 
 def home(request):
     return render(request, 'trecapp/home.html')
 
 def about(request):
     return render(request, 'trecapp/about.html')
-
 
 def uploadRun(request):
 
@@ -32,76 +31,3 @@ def uploadRun(request):
         form = RunForm()
 
     return render(request,'TrecApp/uploadRun.html',{'form': form})
-
-def researcher(request, researcher_name_slug):
-
-	context_dict = {}
-
-	try:
-
-		researcher = Researcher.objects.get(slug=researcher_name_slug)
-
-		context_dict["username"] = researcher.display_name
-		context_dict["name"] = researcher.name
-		context_dict["url"] = researcher.url
-		context_dict["organization"] = researcher.organization
-
-	except Researcher.DoesNotExist:
-		pass
-
-	return render(request, "TrecApp.researcher.html", context_dict)
-
-
-def track(request,track_name_slug): #might need something to usinquely identify tracks?
-
-	context_dict = {}
-
-	try:
-
-		track = Track.objects.get(slug=track_name_slug)
-
-		context_dict["title"] = track.title
-		context_dict["url"] = track.track_url
-		context_dict["description"] = track.description
-		context_dict["genre"] = track.genre
-
-	except Track.DoesNotExist:
-		pass
-
-	return render(request, "TrecApp.track.html", context_dict) #track.html not created yet
-
-def task(request,task_name_slug):
-
-	context_dict = {}
-
-	try:
-
-		task = Task.objects.get(slug=task_name_slug)
-
-		context_dict["title"] = task.title
-		context_dict["task"] = task.track
-		context_dict["description"] = task.description
-		#context_dict["url"] = task.task_url
-		context_dict["year"] = task.year
-		#context_dict["judgement_file"] = task.judgement_file
-
-	except Task.DoesNotExist:
-		pass
-
-	return render(request, "TrecApp.task.html", context_dict) #task.html not created yet
-
-def graph(request, run_name_slug):
-
-	context_dict = {}
-
-	try:
-		run = Run.objects.get(slug=run_name_slug)
-		context_dict["name"] = run.name
-		context_dict["map"] = run.Map
-		context_dict["p10"] = run.p10
-		context_dict["p20"] = run.p20
-
-	except:
-		pass
-
-	return render(request, TrecApp.graph.html, context_dict)
