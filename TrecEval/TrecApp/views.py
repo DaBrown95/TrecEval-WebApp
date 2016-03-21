2dbf5eaf42f4a47c9927c9263f1b60ecd56a823b
Noshin Saiyara <2167932s@student.gla.ac.uk>
2016-03-21 21:33:43 +0000

    PROPER commit : update from master, removing clashes

--- TrecEval/TrecApp/views.py
+++ TrecEval/TrecApp/views.py
@@ -365,15 +365,11 @@ def compareRuns(request):
         form = CompareForm()
 
     return render(request, 'TrecApp/compareRuns.html',{'form': form})
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
 def terms(request):
     return render(request, 'TrecApp/termsandconditions.html')
->>>>>>> master
-=======
+
 
 def terms(request):
     return render(request, 'TrecApp/termsandconditions.html')
->>>>>>> master
+
