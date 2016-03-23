bd676a409472916699dd464949e7bca9ed6a1b51
DaBrown95 <david_a_brown@mac.com>
2016-03-23 19:45:10 +0000

    Fixed profile images

--- TrecEval/TrecApp/views.py
+++ TrecEval/TrecApp/views.py
@@ -132,8 +132,7 @@ def researcher(request, researcher_name_slug):
         context_dict["url"] = researcher.url
         context_dict["organization"] = researcher.organization
         context_dict["runs"] = Run.objects.filter(researcher=researcher).order_by("MAP")
-        # context_dict["user"] = researcher.username
-
+        context_dict["picture"] = researcher.picture
     except Researcher.DoesNotExist:
         pass
 
